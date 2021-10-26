772468d19ccbb61669a329a8f7bfae1092d9f964
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-10-26 05:47:11 -0500

    Model DataTable

---
+++ src/com/pastley/model/DataTable.java
@@ -0,0 +1,62 @@
+package com.pastley.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.primefaces.model.FilterMeta;
+
+public class DataTable<A> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<A> entity;
+	private List<A> select;
+	private List<A> filter;
+	private List<FilterMeta> filterBy;
+
+	public DataTable() {
+		this(new ArrayList<A>());
+	}
+
+	public DataTable(List<A> entity) {
+		this.entity = entity;
+		this.filter = new ArrayList<A>();
+	}
+
+	public List<A> getEntity() {
+		return entity;
+	}
+
+	public void setEntity(List<A> entity) {
+		this.entity = entity;
+	}
+
+	public List<A> getSelect() {
+		return select;
+	}
+
+	public void setSelect(List<A> select) {
+		this.select = select;
+	}
+
+	public List<A> getFilter() {
+		return filter;
+	}
+
+	public void setFilter(List<A> filter) {
+		this.filter = filter;
+	}
+
+	public List<FilterMeta> getFilterBy() {
+		return filterBy;
+	}
+
+	public void setFilterBy(List<FilterMeta> filterBy) {
+		this.filterBy = filterBy;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
