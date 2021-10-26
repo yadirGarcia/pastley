ed6b52fbc8648c8f01223d086f3cc5f28f774002
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-10-26 05:57:13 -0500

    Add template

---
+++ src/com/pastley/util/PastleyVariable.java
@@ -0,0 +1,12 @@
+package com.pastley.util;
+
+import java.io.Serializable;
+
+public class PastleyVariable implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	/* APP */
+	public static final String PASTLEY_APP_NAME = "Pastley";
+
+}
