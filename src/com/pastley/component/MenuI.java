63ed6ed5f122573dfb17825761180e712a4c4774
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-10-29 07:27:35 -0500

    Component MenuI

--- src/com/pastley/component/MenuI.java
+++ src/com/pastley/component/MenuI.java
@@ -3,6 +3,60 @@ package com.pastley.component;
 import java.io.Serializable;
 import java.util.List;
 
-public class MenuI<A> implements Serializable{
-	private List list;
+public class MenuI<A, B> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Item<String>> options;
+	private List<Item<String>> links;
+	private List<Item<A>> level;
+	private List<Item<B>> levelII;
+	private List<Item<String>> levelIII;
+
+	public MenuI() {
+	}
+
+	public List<Item<String>> getOptions() {
+		return options;
+	}
+
+	public void setOptions(List<Item<String>> options) {
+		this.options = options;
+	}
+
+	public List<Item<String>> getLinks() {
+		return links;
+	}
+
+	public List<Item<A>> getLevel() {
+		return level;
+	}
+
+	public void setLevel(List<Item<A>> level) {
+		this.level = level;
+	}
+
+	public List<Item<B>> getLevelII() {
+		return levelII;
+	}
+
+	public void setLevelII(List<Item<B>> levelII) {
+		this.levelII = levelII;
+	}
+
+	public List<Item<String>> getLevelIII() {
+		return levelIII;
+	}
+
+	public void setLevelIII(List<Item<String>> levelIII) {
+		this.levelIII = levelIII;
+	}
+
+	public void setLinks(List<Item<String>> links) {
+		this.links = links;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
