1fda3a85d339cd69b3ca4884f54d6b7b67a8b199
SerBuitrago <sergiostivesbb@ufps.edu.co>
2021-11-06 06:42:05 -0500

    Fill card user

---
+++ src/com/pastley/bean/view/CardBean.java
@@ -0,0 +1,43 @@
+package com.pastley.bean.view;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.pastley.component.Card;
+
+import lombok.Data;
+
+@Data
+@ManagedBean(name = "card")
+@ViewScoped
+public class CardBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Card> cardsUser;
+	
+	public CardBean(){
+	}
+	
+	@PostConstruct
+	public void init() {
+		this.cardsUser = new ArrayList<Card>();
+	}
+	
+	public void initCardUser() {
+		for(int i= 1; i<=6; i++) {
+			Card card = new Card("login.xhtml", "30000"+i, "Card "+i);
+			card.setStyleClass("lsy-bg-success");
+			card.setId("Card "+i);
+			card.setIcon("pi pi-user");
+			this.cardsUser.add(card);
+		}
+	}
+	
+
+}
